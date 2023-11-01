d2ca4248c8c845802b4b2f871f0fffaadc8fb478
Ludwig <ludwig@frostudios.com>
2023-11-01 12:32:08 +0100

    Playground for executing files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+#include "./lectures/lecture1/minutesToSeconds.h"
+#include "./lectures/lecture1/convertMilesToKilometer.h"
+#include "./lectures/lecture1/multiplication.h"
+#include "./lectures/lecture1/division.h"
+#include "./lectures/lecture1/canDivideBySeven.h"
+
+using namespace std;
+
+int main(){
+
+    canDivideBySeven(14);
+    return 0;
+}
