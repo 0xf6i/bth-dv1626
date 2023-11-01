1a0c9482a37777d953fd6b7cec8c5a8418223da2
Ludwig <ludwig@frostudios.com>
2023-11-01 12:22:13 +0100

    4. Låt användaren mata in 2 heltal och beräkna och presentera kvoten (division) av talen. Ex inmatning av 10 och 4 ger resultatet 2.5

---
+++ lectures/lecture1/division.h
@@ -0,0 +1,18 @@
+//
+// Created by Ludwig Sterner on 2023-11-01.
+//
+
+#ifndef BTH_DV1626_DIVISION_H
+#define BTH_DV1626_DIVISION_H
+
+
+float division(float a, float b){
+    float dividedProduct;
+
+    dividedProduct = a / b;
+
+    return dividedProduct;
+}
+
+
+#endif //BTH_DV1626_DIVISION_H
