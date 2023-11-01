206bc6174ad4f19565344d961b8e7b267ae90f22
Ludwig <ludwig@frostudios.com>
2023-11-01 12:08:40 +0100

    1. Bestäm totala antalet sekunder för 50 minuter och 25 sekunder.

---
+++ lectures/lecture1/minutesToSeconds.h
@@ -0,0 +1,20 @@
+//
+// Created by Ludwig Sterner on 2023-11-01.
+//
+
+#ifndef BTH_DV1626_MINUTESTOSECONDS_H
+#define BTH_DV1626_MINUTESTOSECONDS_H
+
+
+int minutesToSeconds(int minutes, int seconds) {
+
+    int totalSeconds;
+    totalSeconds = minutes * 60;
+    totalSeconds += seconds;
+
+    return totalSeconds;
+
+};
+
+
+#endif //BTH_DV1626_MINUTESTOSECONDS_H
