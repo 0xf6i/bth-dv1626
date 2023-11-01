218a888202c406924dfd8897cb932c1da68ff8df
Ludwig <ludwig@frostudios.com>
2023-11-01 12:16:03 +0100

    3. Låt användaren mata in 2 heltal och beräkna och presentera produkten (multiplikation) av talen. Ex. inmatning av 6 och 4 ger resultatet 24

---
+++ lectures/lecture1/multiplication.h
@@ -0,0 +1,17 @@
+//
+// Created by Ludwig Sterner on 2023-11-01.
+//
+
+#ifndef BTH_DV1626_MULTIPLICATION_H
+#define BTH_DV1626_MULTIPLICATION_H
+
+
+int multiplication(int a, int b){
+    int product;
+
+    product = a * b;
+    return product;
+}
+
+
+#endif //BTH_DV1626_MULTIPLICATION_H
