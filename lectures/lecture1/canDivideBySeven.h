65bf34ae8215a19f0d953233795c4b1d87430de7
Ludwig <ludwig@frostudios.com>
2023-11-01 12:31:21 +0100

    5. Låt användaren mata in ett heltal och avgör om heltalet är jämnt delbart med 7.

---
+++ lectures/lecture1/canDivideBySeven.h
@@ -0,0 +1,26 @@
+//
+// Created by Ludwig Sterner on 2023-11-01.
+//
+
+#ifndef BTH_DV1626_CANDIVIDEBYSEVEN_H
+#define BTH_DV1626_CANDIVIDEBYSEVEN_H
+
+// CODE BENEATH
+
+void canDivideBySeven(int a){
+    bool status;
+
+    // simplified if-statement (ternary operator)
+    a % 7 == 0 ? status = true : status = false;
+
+
+    if (status){
+        std::cout << "Can divide by seven";
+    }
+    else {
+        std::cout << "Cannot divide by seven";
+    }
+}
+
+
+#endif //BTH_DV1626_CANDIVIDEBYSEVEN_H
