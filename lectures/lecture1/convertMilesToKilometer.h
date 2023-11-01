1494d4786c664a0e7daeca935e34e740ca240b64
Ludwig <ludwig@frostudios.com>
2023-11-01 12:13:24 +0100

    2. Bestäm hur många miles 15 km motsvarar. (1 mile är 1.609344 km)

---
+++ lectures/lecture1/convertMilesToKilometer.h
@@ -0,0 +1,17 @@
+//
+// Created by Ludwig Sterner on 2023-11-01.
+//
+
+#ifndef BTH_DV1626_CONVERTMILESTOKILOMETER_H
+#define BTH_DV1626_CONVERTMILESTOKILOMETER_H
+
+
+float convertMilesToKilometer(int miles){
+    float mileLength = 1.609344;
+    float convertedMiles = miles * mileLength;
+
+    return convertedMiles;
+}
+
+
+#endif //BTH_DV1626_CONVERTMILESTOKILOMETER_H
