19cd745a367413915442dc2a90e9e050f3e1b653
Ludwig <ludwig@frostudios.com>
2023-11-02 17:53:56 +0100

    1. Gör ett program som med hjälp av en for-loop skriver ut alla heltal från 20 till 10

---
+++ lectures/lecture2/countdown.h
@@ -0,0 +1,20 @@
+//
+// Created by Ludwig Sterner on 2023-11-02.
+//
+
+#ifndef BTH_DV1626_COUNTDOWN_H
+#define BTH_DV1626_COUNTDOWN_H
+#include <iostream>
+
+using namespace std;
+
+void countdown() {
+
+    for (int i = 20; i >= 10; i--){
+        cout << i << ", ";
+    }
+
+};
+
+
+#endif //BTH_DV1626_COUNTDOWN_H
