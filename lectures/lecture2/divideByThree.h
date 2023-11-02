c0ea8ca433dce9c2711c24eff473cdbc105f40a3
Ludwig <ludwig@frostudios.com>
2023-11-02 17:56:35 +0100

    2. Gör ett program som med hjälp av en for-loop skriver ut alla heltal fr.o.m 33 t.o.m 99 som är jämnt delbara med 3.

---
+++ lectures/lecture2/divideByThree.h
@@ -0,0 +1,22 @@
+//
+// Created by Ludwig Sterner on 2023-11-02.
+//
+
+#ifndef BTH_DV1626_DIVIDEBYTHREE_H
+#define BTH_DV1626_DIVIDEBYTHREE_H
+#include <iostream>
+
+using namespace std;
+
+void divideByThree() {
+
+    for(int i = 33; i <= 99; i++){
+        if (i % 3 == 0){
+            cout << i << endl;
+        }
+    }
+
+};
+
+
+#endif //BTH_DV1626_DIVIDEBYTHREE_H
