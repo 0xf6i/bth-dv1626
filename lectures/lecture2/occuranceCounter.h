d0f22fca2ad0489b68f9b513a15112882f1a0189
Ludwig <ludwig@frostudios.com>
2023-11-04 00:18:56 +0100

    7. Gör ett program som simulerar ett antal tärningskast (vilka skrivs ut) och dessutom beräknar antalet förekomster av varje tärningsvärde. Slutligen presenteras resultatet som ett liggande histogram.

---
+++ lectures/lecture2/occuranceCounter.h
@@ -0,0 +1,44 @@
+//
+// Created by Ludwig Sterner on 2023-11-03.
+//
+
+#ifndef BTH_DV1626_OCCURANCECOUNTER_H
+#define BTH_DV1626_OCCURANCECOUNTER_H
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+void occuranceCounter() {
+
+    int counts[6] = {0};
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> distribution(1, 6);
+
+    int amountRuns;
+
+    cout << "How many rolls? ", cin >> amountRuns;
+
+    int randomNumber = distribution(gen);
+
+    for (int i = 0; i < amountRuns; i++){
+        int randomNumber = distribution(gen);
+        counts[randomNumber - 1]++;
+        cout << randomNumber << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < 6; ++i) {
+        cout << i + 1 << ": ";
+        for (int j = 0; j < counts[i]; ++j) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+
+};
+
+
+#endif //BTH_DV1626_OCCURANCECOUNTER_H
