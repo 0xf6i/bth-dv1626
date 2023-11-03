98bbf634887f8ef350f037f7b9b067f262711f81
Ludwig <ludwig@frostudios.com>
2023-11-03 14:20:42 +0100

    4. Låt använvändaren mata in ett årtal och avgör om detta årtal är ett skottår. Ett skottår definieras av att det är jämnt delbart med 4 men inte jämnt delbart med 100.

---
+++ lectures/lecture2/leapYearChecker.h
@@ -0,0 +1,20 @@
+//
+// Created by Ludwig Sterner on 2023-11-03.
+//
+
+#ifndef BTH_DV1626_LEAPYEARCHECKER_H
+#define BTH_DV1626_LEAPYEARCHECKER_H
+#include <iostream>
+
+using namespace std;
+
+void leapYearChecker() {
+
+    int year;
+    cout << "What year would you like to check? ", cin >> year;
+    (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) ? cout << year << " is a leap year." : cout << year << " is not a leap year.";
+
+};
+
+
+#endif //BTH_DV1626_LEAPYEARCHECKER_H
